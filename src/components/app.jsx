5829f60c23946051db428674a2c66c8ffa17992d
ubiregi-tatsuya-ishii <tatsuya.ishii@ubiregi.com>
2019-12-28 21:14:24 +0900

    clean up project

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import Greeting from './greeting';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Bob'
     }
-  }
-
-  handleNameChange(name) {
-    this.setState({name});
   }
 
   render() {
     return (
       <div>
-        <input type="text"
-               value={this.state.name}
-               onChange={e => this.handleNameChange(e.target.value)}
-        />
-        <Greeting name={this.state.name}/>
       </div>
     )
   }
