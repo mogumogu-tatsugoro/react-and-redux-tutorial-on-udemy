6b66d960d8e58a14af56064509d853cd9cfab099
ubiregi-tatsuya-ishii <tatsuya.ishii@ubiregi.com>
2019-12-28 21:50:57 +0900

    create SearchForm component prototype

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import SearchForm from './SearchForm'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  handlePlaceSubmit(place) {
+    console.log(place)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={
+          place => this.handlePlaceSubmit(place)
+        } />
+      </div>
+    )
+  }
+}
