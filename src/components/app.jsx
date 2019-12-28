b0e5422b3b0826e19da5248b82eb0c57571ea46c
ubiregi-tatsuya-ishii <tatsuya.ishii@ubiregi.com>
2019-12-28 14:54:10 +0900

    set event handler to change state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Bob'
+    }
+  }
+
+  handleMouseOver() {
+    this.setState({
+      name: 'John'
+    });
+  }
+
+  handleMouseOut() {
+    this.setState({
+      name: 'Tom'
+    });
+  }
+
   render() {
     return (
-      <div>
-        <Greeting name='john' />
-        <Greeting name='bob' />
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name}/>
       </div>
     )
   }
